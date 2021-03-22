a3ba7283ac06b39053367c7e4d0d98e88ab337c2
alessandro <finialessandro90@gmail.com>
2021-03-22 14:42:23 +0100

    table created

--- database/migrations/2021_03_22_132724_create_passengers_table.php
+++ database/migrations/2021_03_22_132724_create_passengers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePassengersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('passengers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->date('birth_date');
+            $table->string('address');
+            $table->string('country');
+            $table->string('phone');
+            $table->string('passenger_id');
+            $table->string('train_car');
+            $table->float('price', 6,2);
             $table->timestamps();
         });
     }
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('passengers');
     }
 }
