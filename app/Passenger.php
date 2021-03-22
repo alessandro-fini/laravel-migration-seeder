84b96de7189dd48cbc02cd19f84e16d97265661e
alessandro <finialessandro90@gmail.com>
2021-03-22 14:46:51 +0100

    fillable

--- app/Passenger.php
+++ app/Passenger.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Passenger extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'birth_date',
+        'addess',
+        'country',
+        'phone',
+        'passenger_id',
+        'train_car',
+        'price'
+    ];
 }
